1a9f9ce17c4ce95caa1c9b325b0adaed895cdc56
Marco Ricci <pumavizioso@gmail.com>
2021-05-05 12:10:00 +0100

    add prev and next handler

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ const formContainer = document.querySelector('.search-container');
 const gallery = document.querySelector('.gallery');
 
 let employees = [];
+let indexModalWindow = null;
 
 // Create the form
 const formHTML = `
@@ -59,37 +60,69 @@ function generateModalWindow(index) {
 
   // Create the modal window
   const modalHTML = `
-<div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src=${picture.large} alt="profile picture">
-            <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
-            <p class="modal-text">${email}</p>
-            <p class="modal-text cap">${location.city}</p>
-            <hr>
-            <p class="modal-text">${phone}</p>
-            <p class="modal-text">${location.street.number} ${location.street.name}, ${location.city},  ${location.postcode}</p>
-            <p class="modal-text">Birthday: ${day}/${month}/${year}</p>
+      <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+                <p class="modal-text">${email}</p>
+                <p class="modal-text cap">${location.city}</p>
+                <hr>
+                <p class="modal-text">${phone}</p>
+                <p class="modal-text">${location.street.number} ${location.street.name}, ${location.city},  ${location.postcode}</p>
+                <p class="modal-text">Birthday: ${day}/${month}/${year}</p>
+            </div>
+            <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>
         </div>
-    </div>
-`;
+      `;
 
   // Insert the modal window in the body
   document.body.insertAdjacentHTML('beforeend', modalHTML);
+
+  // Get the elements in the modal window
   const closeBtn = document.getElementById('modal-close-btn');
+  const nextBtn = document.getElementById('modal-next');
+  const prevBtn = document.getElementById('modal-prev');
+  const modalWindow = document.querySelector('.modal-container');
 
   //   Handler close button
   closeBtn.addEventListener('click', () => {
-    //   target the div
-    const el = document.querySelector('.modal-container');
-    // remove the div
-    el.remove();
+    modalWindow.remove();
+  });
+
+  // Handler Next button
+  nextBtn.addEventListener('click', () => {
+    modalWindow.remove();
+
+    if (indexModalWindow >= employees.length - 1) {
+      indexModalWindow = 0;
+      generateModalWindow(indexModalWindow);
+    } else {
+      indexModalWindow++;
+      generateModalWindow(indexModalWindow);
+    }
+  });
+
+  // Handler Prev button
+  prevBtn.addEventListener('click', () => {
+    modalWindow.remove();
+
+    if (indexModalWindow === 0) {
+      indexModalWindow = employees.length - 1;
+      generateModalWindow(indexModalWindow);
+    } else {
+      indexModalWindow--;
+      generateModalWindow(indexModalWindow);
+    }
   });
 }
 
 // Handler open modal window
-
 gallery.addEventListener('click', (e) => {
   // Check if the clicked element is a card
   if (e.target !== gallery) {
@@ -97,13 +130,14 @@ gallery.addEventListener('click', (e) => {
     const card = e.target.closest('.card');
     // Get the card data-index
     const index = card.getAttribute('data-index');
+    // Store the index in the variable
+    indexModalWindow = index;
     // call the generate modal function passing the data-index as argument
     generateModalWindow(index);
   }
 });
 
 // Filter Handler
-
 function searchEmployee(input) {
   const filteredEmployees = [];
 
@@ -129,5 +163,3 @@ form.addEventListener('submit', (e) => {
   const input = document.getElementById('search-input');
   searchEmployee(input.value);
 });
-
-// Clear filter handler
